6aea9614d90121629b8738d0bacfd7486fb8923d
lulubun <laurapetitt@gmail.com>
2017-11-17 16:53:06 -0500

    worked on put requests

--- discuss/web/controllers/topic_controller.ex
+++ discuss/web/controllers/topic_controller.ex
@@ -4,7 +4,9 @@ defmodule Discuss.TopicController do
     alias Discuss.Topic
 
     def index(conn, _params) do
-        Repo.all(Topic)
+        IO.inspect(conn)
+        topics = Repo.all(Topic)
+        render conn, "index.html", topics: topics
     end
 
     def new(conn, _params) do
@@ -17,9 +19,32 @@ defmodule Discuss.TopicController do
         changeset = Topic.changeset(%Topic{}, topic)
 
         case Repo.insert(changeset) do
-            {:ok, post} -> IO.inspect(post)
+            {:ok, _topic} ->
+                conn
+                |> put_flash(:info, "Topic Created")
+                |> redirect(to: topic_path(conn, :index))
             {:error, changeset} -> 
                 render conn, "new.html", changeset: changeset
+        end
+    end
+
+    def edit(conn, %{"id" => topic_id}) do
+        topic = Repo.get(Topic, topic_id)
+        changeset = Topic.changeset(topic)
+
+        render conn, "edit.html", changeset: changeset, topic: topic
+    end
+
+    def update(conn, %{"id" => topic_id, "topic" => topic}) do
+        changeset = Repo.get(Topic, topic_id) |> Topic.changeset(topic)
+
+        case Repo.update(changeset) do
+            {:ok, _topic} ->
+                conn
+                |> put_flash(:info, "Topic Updated")
+                |> redirect(to: topic_path(conn, :index))
+            {:error, changeset} -> 
+                render conn, "edit.html", changeset: changeset
         end
     end
 end
